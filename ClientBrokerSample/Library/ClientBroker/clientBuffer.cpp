fdfd0f8f0d140cce8f9532df29e5597bdcb5d1ad
ccceeefff <cef.ramirez@gmail.com>
2016-04-13 13:56:04 -0700

    adding register call and disconnect

--- ClientBrokerSample/Library/ClientBroker/clientBuffer.cpp
+++ ClientBrokerSample/Library/ClientBroker/clientBuffer.cpp
@@ -12,6 +12,7 @@ void jsonDecode(char json[]);
 void jsonAndSend(payload_t buffMsgs[], String pdid, String appid);
 String connectandsend(String data,String path,String servername);
 void Wifi_init(String Networkname, String password);
+void registerWithServer(String serverName, String port, String appId, String dueId);
 static void sendTask(void *arg);
 
 
@@ -77,6 +78,7 @@ void clientBuffer::initialize(String appID, String powerID, String net, String p
   xSendQueue = xQueueCreate(QUEUE_SIZE, sizeof(payload_t));
   //xReceiveQueue = xQueueCreate(QUEUE_SIZE, sizeof(String));
   Wifi_init(net, pass);
+  registerWithServer("192.168.43.12", "3001", appID, powerID);
   xTaskCreate(sendTask, NULL, STACK_SIZE, NULL, 1, NULL);
   // Create send and Receive Tasks  
 }
@@ -102,6 +104,108 @@ void clientBuffer::publish(String port, char *Message)
 
 /************************************
  ***** HELPER FUNCTIONS *************/
+
+void registerWithServer(String serverName, String port, String appId, String dueId){
+  char a;
+  int i=0;
+  char c;
+  String path = "/register/" + dueId + "/" + appId;
+  
+  Serial1.println("AT+CIPSTART=\"TCP\",\"" + serverName + "\"," + port); 
+  while(true){
+    if(Serial1.available()){
+      a=Serial1.read();
+      SerialUSB.print(a);
+      }
+      if(a=='O'){
+        i=1;
+      }
+      if(a=='K'&&i==1){
+        i=3;
+        break;
+      }
+  }
+  i=0;
+  delayMS(150);
+  String data = "";
+  String t1 = "POST "+path+" HTTP/1.1\r\nHost:"+serverName+":" + port +"\r\nContent-Type:application/json\r\nContent-Length:"+String(data.length())+"\r\n";
+  //String t3 = "{\"data\":[[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[31,1],[32,3],[33,3],[34,3],[35,3],[36,0],[37,1],[38,2],[39,3],[40,3],[41,3],[42,5],[43,11],[44,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3]],\"sampleRate\":44000}";
+  SerialUSB.println(data.length());
+  SerialUSB.println(t1.length()+data.length()+4);
+  Serial1.print("AT+CIPSEND=");
+  Serial1.println(t1.length()+data.length()+4);
+  SerialUSB.println("t1:");
+  SerialUSB.println(t1);
+  while(true){
+       if(Serial1.available()){
+    a=Serial1.read();
+    SerialUSB.print(a);
+    }
+    if(a=='>'){
+      break;
+    }
+  }
+  
+  Serial1.println(t1);
+  //Serial1.println(t2);
+  Serial1.println(data);
+  while(true){
+    if(Serial1.available()){
+      a=Serial1.read();
+      SerialUSB.print(a);
+    }
+    if(a=='O'){
+      i=1;
+    }
+    if(a=='K'&&i==1){
+     i=3;
+      break;
+    }
+     
+  }
+  i=0;
+  int x=0;
+  SerialUSB.println("");
+  SerialUSB.println("Registered!!!");
+  while(true){
+    if(Serial1.available()){
+      a=Serial1.read();
+      SerialUSB.print(a);
+    }
+    if(a=='v'){
+      x=i;
+    }
+    if(a=='e'&&i==x+1){
+      break;
+    }
+    i++;
+  }
+ 
+  SerialUSB.println("done!!");
+  // not expecting any response
+  i=0;
+  String str;
+  // flush response
+  while(Serial1.available()){
+    a=Serial1.read();
+    SerialUSB.print(a);
+  }
+  Serial1.println("AT+CIPCLOSE");
+  while(true){
+    if(Serial1.available()){
+      a=Serial1.read();
+      SerialUSB.print(a);
+    }
+    if(a=='O'){
+      i=1;
+    }
+    if(a=='K'&&i==1){
+     i=3;
+      break;
+    }
+  }
+  SerialUSB.println("Connection closed");
+}
 
 String connectandsend(String data,String path,String servername){
   char a;
@@ -195,9 +299,22 @@ String connectandsend(String data,String path,String servername){
       break;
     }
     i++;
-    
-     
   }
+  Serial1.println("AT+CIPCLOSE");
+  while(true){
+    if(Serial1.available()){
+      a=Serial1.read();
+      SerialUSB.print(a);
+    }
+    if(a=='O'){
+      i=1;
+    }
+    if(a=='K'&&i==1){
+     i=3;
+      break;
+    }
+  }
+  SerialUSB.println("Connection closed");
   return str;
 }
 
@@ -230,7 +347,7 @@ void jsonAndSend(payload_t buffMsgs[], String pdid, String appid) {
   endpoint.concat(pdid);
   endpoint.concat("/");
   endpoint.concat(appid);
-  String response = connectandsend(String(buffer), endpoint, "10.215.1.12");
+  String response = connectandsend(String(buffer), endpoint, "192.168.43.12");
 
   // Free here
   for (int i = 0; i < BUFF_SIZE; i++) {
@@ -302,7 +419,7 @@ void Wifi_init(String Networkname,String password){
   Serial1.begin(115200);
   SerialUSB.println("Serial1 begin");
   delayMS(1000);
-  Serial1.println("AT");
+  Serial1.println("AT+RST");
   while(true){
    if(Serial1.available()){
       a=Serial1.read();
