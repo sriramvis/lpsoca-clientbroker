690918297ab88d58e026f9e5cd77b869acd6febd
ccceeefff <cef.ramirez@gmail.com>
2016-04-14 12:34:08 -0700

    enable/disable debug logs

--- ClientBrokerSample/Library/ClientBroker/clientBuffer.cpp
+++ ClientBrokerSample/Library/ClientBroker/clientBuffer.cpp
@@ -5,6 +5,15 @@
 #define STACK_SIZE 800
 #define JBUFF_SIZE 500 // Buffer size for JSON decoding
 
+#define DEBUG 0
+#if DEBUG
+  #define vDebug(x) SerialUSB.print(x)
+  #define vDebugln(x) SerialUSB.println(x)
+#else
+  #define vDebug(x)
+  #define vDebugln(x)
+#endif
+
 QueueHandle_t xSendQueue = NULL;
 QueueHandle_t xReceiveQueue = NULL;
 
@@ -31,7 +40,7 @@ static void delayMS(uint32_t millis) {
 }
 
 static void sendTask(void *arg) {
-  SerialUSB.println("sendTask started");
+  vDebugln("sendTask started");
 
   payload_t buffMsgs[BUFF_SIZE];
 
@@ -42,17 +51,17 @@ static void sendTask(void *arg) {
     //SerialUSB.println("Starting While loop in send");
     //payload_t toSend;
     xQueueReceive(xSendQueue, &toSend, portMAX_DELAY);
-    SerialUSB.println("Received from Queue");
-    SerialUSB.println(toSend.port);
+    vDebugln("Received from Queue");
+    vDebugln(toSend.port);
     buffMsgs[counter] = toSend;
 
     if(counter >= BUFF_SIZE - 1) {
       counter = 0; // reset counter
-      SerialUSB.println("About to call jsonAndSend");
+      vDebugln("About to call jsonAndSend");
       jsonAndSend(buffMsgs, *_powerID, *_appID);
       // Send the messages
     } else {
-      SerialUSB.println("Increasing Counter");
+      vDebugln("Increasing Counter");
       counter++;
     }
   }
@@ -98,14 +107,13 @@ void clientBuffer::publish(int port, char *Message)
   payload.port = port;
   payload.message = new String(Message);
 
-
   if(xQueueSendToBack(xSendQueue, &payload, portMAX_DELAY) != pdTRUE) {
-    SerialUSB.println("Unable to place on Queue. Queue full");
+    vDebugln("Unable to place on Queue. Queue full");
     // Deal with this case
     // If this happens then the queue is full
     // Should never happen though
   }
-  SerialUSB.println("Published on Queue");
+  vDebugln("Published on Queue");
 }
 
 void clientBuffer::subscribe(int port, QueueHandle_t receiveQueue){
@@ -127,7 +135,7 @@ void registerWithServer(String serverName, String port, String appId, String due
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
       }
       if(a=='O'){
         i=1;
@@ -142,16 +150,16 @@ void registerWithServer(String serverName, String port, String appId, String due
   String data = "";
   String t1 = "POST "+path+" HTTP/1.1\r\nHost:"+serverName+":" + port +"\r\nContent-Type:application/json\r\nContent-Length:"+String(data.length())+"\r\n";
   //String t3 = "{\"data\":[[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[31,1],[32,3],[33,3],[34,3],[35,3],[36,0],[37,1],[38,2],[39,3],[40,3],[41,3],[42,5],[43,11],[44,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3]],\"sampleRate\":44000}";
-  SerialUSB.println(data.length());
-  SerialUSB.println(t1.length()+data.length()+4);
+  vDebugln(data.length());
+  vDebugln(t1.length()+data.length()+4);
   Serial1.print("AT+CIPSEND=");
   Serial1.println(t1.length()+data.length()+4);
-  SerialUSB.println("t1:");
-  SerialUSB.println(t1);
+  vDebugln("t1:");
+  vDebugln(t1);
   while(true){
        if(Serial1.available()){
     a=Serial1.read();
-    SerialUSB.print(a);
+    vDebug(a);
     }
     if(a=='>'){
       break;
@@ -164,7 +172,7 @@ void registerWithServer(String serverName, String port, String appId, String due
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -177,12 +185,12 @@ void registerWithServer(String serverName, String port, String appId, String due
   }
   i=0;
   int x=0;
-  SerialUSB.println("");
-  SerialUSB.println("Registered!!!");
+  vDebugln("");
+  vDebugln("Registered!!!");
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='v'){
       x=i;
@@ -193,20 +201,20 @@ void registerWithServer(String serverName, String port, String appId, String due
     i++;
   }
  
-  SerialUSB.println("done!!");
+  vDebugln("done!!");
   // not expecting any response
   i=0;
   String str;
   // flush response
   while(Serial1.available()){
     a=Serial1.read();
-    SerialUSB.print(a);
+    vDebug(a);
   }
   Serial1.println("AT+CIPCLOSE");
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -216,7 +224,7 @@ void registerWithServer(String serverName, String port, String appId, String due
       break;
     }
   }
-  SerialUSB.println("Connection closed");
+  vDebugln("Connection closed");
 }
 
 String connectandsend(String data,String path,String servername){
@@ -230,7 +238,7 @@ String connectandsend(String data,String path,String servername){
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
       }
       if(a=='O'){
         i=1;
@@ -244,16 +252,16 @@ String connectandsend(String data,String path,String servername){
   delayMS(150);
   String t1 = "POST "+path+" HTTP/1.1\r\nHost:"+servername+":3001"+"\r\nContent-Type:application/json\r\nContent-Length:"+String(data.length())+"\r\n";
   //String t3 = "{\"data\":[[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[31,1],[32,3],[33,3],[34,3],[35,3],[36,0],[37,1],[38,2],[39,3],[40,3],[41,3],[42,5],[43,11],[44,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3],[9,3],[10,3],[11,5],[12,11],[13,5],[14,6],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],[21,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[0,1],[1,3],[2,3],[3,3],[4,3],[5,0],[6,1],[7,2],[8,3]],\"sampleRate\":44000}";
-  SerialUSB.println(data.length());
-  SerialUSB.println(t1.length()+data.length()+4);
+  vDebugln(data.length());
+  vDebugln(t1.length()+data.length()+4);
   Serial1.print("AT+CIPSEND=");
   Serial1.println(t1.length()+data.length()+4);
-  SerialUSB.println("t1:");
-  SerialUSB.println(t1);
+  vDebugln("t1:");
+  vDebugln(t1);
   while(true){
        if(Serial1.available()){
     a=Serial1.read();
-    SerialUSB.print(a);
+    vDebug(a);
     }
     if(a=='>'){
       break;
@@ -268,7 +276,7 @@ String connectandsend(String data,String path,String servername){
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -281,12 +289,12 @@ String connectandsend(String data,String path,String servername){
   }
   i=0;
   int x=0;
-  SerialUSB.println("");
-  SerialUSB.println("Working!!!");
+  vDebugln("");
+  vDebugln("Working!!!");
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='v'){
       x=i;
@@ -297,7 +305,7 @@ String connectandsend(String data,String path,String servername){
     i++;
   }
  
-  SerialUSB.println("done!!");
+  vDebugln("done!!");
   i=0;
   String str;
   while(true){
@@ -316,7 +324,7 @@ String connectandsend(String data,String path,String servername){
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -326,23 +334,21 @@ String connectandsend(String data,String path,String servername){
       break;
     }
   }
-  SerialUSB.println("Connection closed");
+  vDebugln("Connection closed");
   return str;
 }
 
 void jsonAndSend(payload_t buffMsgs[], String pdid, String appid) {
   StaticJsonBuffer<500> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
-  root["pdid"] = pdid;
-  root["appid"] = appid;
 
   // JsonArray& port = root.createNestedArray("port");
   JsonArray& msg = root.createNestedArray("msg");
 
   for (int i = 0; i < BUFF_SIZE; i++) {
     JsonArray& m = msg.createNestedArray();
-    SerialUSB.println(buffMsgs[i].port);
-    SerialUSB.println(*(buffMsgs[i].message));
+    vDebugln(buffMsgs[i].port);
+    vDebugln(*(buffMsgs[i].message));
     m.add(buffMsgs[i].port);
     m.add(*(buffMsgs[i].message));
   }
@@ -351,8 +357,8 @@ void jsonAndSend(payload_t buffMsgs[], String pdid, String appid) {
 
   root.printTo(buffer, sizeof(buffer));
 
-  SerialUSB.println("About to send json up");
-  SerialUSB.println(buffer);
+  vDebugln("About to send json up");
+  vDebugln(buffer);
 
   // String response = connectandsend(String(buffer), "/power/45", "54.191.239.210");
   String endpoint = String("/up/");
@@ -366,7 +372,7 @@ void jsonAndSend(payload_t buffMsgs[], String pdid, String appid) {
     delete (buffMsgs[i].message);
   }
 
-  SerialUSB.println("Received Response. About to decode");
+  vDebugln("Received Response. About to decode");
 
   response.toCharArray(buffer, JBUFF_SIZE);
 
@@ -384,19 +390,19 @@ inline void notifyPortQueue(int port, String message){
 }
 
 void jsonDecode(char json[]) {
-  SerialUSB.println("Entering Json Decoding");
-  SerialUSB.println(json);
+  vDebugln("Entering Json Decoding");
+  vDebugln(json);
   StaticJsonBuffer<500> jsonBuffer;
   //String ports[10];
   //String messages[10];
   JsonObject& root = jsonBuffer.parseObject(json);
 
   if (!root.success()) {
-    SerialUSB.println("parseObject() in json decoder failed");
+    vDebugln("parseObject() in json decoder failed");
     return;
   } else {
-    SerialUSB.println("parsed Object:");
-    root.printTo(SerialUSB);
+    vDebugln("parsed Object:");
+    //root.printTo(SerialUSB);
 
     // get msg array
     JsonArray& msgs = root["msg"];
@@ -407,11 +413,11 @@ void jsonDecode(char json[]) {
         int port = m[0];
         String message = m[1];
         notifyPortQueue(port, message);
-        /*
-        SerialUSB.print("port: ");
-        SerialUSB.println(port);
-        SerialUSB.println("message: " + message);
-        */
+        
+        vDebug("port: ");
+        vDebugln(port);
+        vDebugln("message: " + message);
+        
       }
     }
 
@@ -440,7 +446,7 @@ void jsonDecode(char json[]) {
 }
 
 void Wifi_init(String Networkname,String password){
-  SerialUSB.println("1");
+  vDebugln("1");
   char a;
   int i=0;
   pinMode(40,OUTPUT);
@@ -453,15 +459,15 @@ void Wifi_init(String Networkname,String password){
   digitalWrite(36,HIGH);
   delayMS(500);
  
-  SerialUSB.println("1");
+  vDebugln("1");
   Serial1.begin(115200);
-  SerialUSB.println("Serial1 begin");
+  vDebugln("Serial1 begin");
   delayMS(1000);
   Serial1.println("AT+RST");
   while(true){
    if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -477,7 +483,7 @@ void Wifi_init(String Networkname,String password){
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -493,7 +499,7 @@ void Wifi_init(String Networkname,String password){
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     
     if(a=='O'){
@@ -516,7 +522,7 @@ void Wifi_init(String Networkname,String password){
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -538,7 +544,7 @@ void Wifi_init(String Networkname,String password){
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -560,7 +566,7 @@ void Wifi_init(String Networkname,String password){
   while(true){
     if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -578,7 +584,7 @@ void Wifi_init(String Networkname,String password){
  while(true){
    if(Serial1.available()){
     a=Serial1.read();
-    SerialUSB.print(a);
+    vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -596,7 +602,7 @@ void Wifi_init(String Networkname,String password){
  while(true){
    if(Serial1.available()){
       a=Serial1.read();
-      SerialUSB.print(a);
+      vDebug(a);
     }
     if(a=='O'){
       i=1;
@@ -608,8 +614,8 @@ void Wifi_init(String Networkname,String password){
   }
   i=0;
   
-  SerialUSB.println("");
-  SerialUSB.println("Connected!!!");
+  vDebugln("");
+  vDebugln("Connected!!!");
 }
 
 
